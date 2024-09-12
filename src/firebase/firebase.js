a171b4ed6627a3495d192aed3c5d13daae502337
justinpope99 <115605273+justinpope99@users.noreply.github.com>
2024-09-12 17:22:21 -0400

    Modify Firebase configuration to use a new Firebase project

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // This is our configuration. It tells our code that this is the Firebase project that we want to connect to. We're passing this configuration into initializeApp(), and it returns the firebase application we have on the cloud.
 const firebaseConfig = {
-  apiKey: "AIzaSyC9u7zfjqU6fIQB9A0cqDsgdSR1zWHGRN8",
-  authDomain: "instagram-clone-9dc39.firebaseapp.com",
-  projectId: "instagram-clone-9dc39",
-  storageBucket: "instagram-clone-9dc39.appspot.com",
-  messagingSenderId: "335880814708",
-  appId: "1:335880814708:web:e34921c6e79ddb0ba69121",
-  measurementId: "G-37JN7PFP73",
+  apiKey: "AIzaSyAaocl1E7H1rM2Wq4_iFVA8dU0zTwz20eo",
+  authDomain: "justin-instagram-clone.firebaseapp.com",
+  projectId: "justin-instagram-clone",
+  storageBucket: "justin-instagram-clone.appspot.com",
+  messagingSenderId: "279137016961",
+  appId: "1:279137016961:web:0e5e843c22da439da2c180",
+  measurementId: "G-R2VP7Q5YWT",
 };
 
 const app = initializeApp(firebaseConfig);
